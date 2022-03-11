195e8b3d5ba3f97d1d6ccd7beb927767bdf92ec4
ppopow <pawel.popow@gmail.com>
2022-03-11 21:24:19 +0000

    str

--- intro/urls.py
+++ intro/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('home/', views.home),
     path('home2/', views.home2),
     path('home3/', views.home3),
+    path('home4/', views.home4),
     path('<str:name>/', views.name_view),
 ]
 
--- intro/views.py
+++ intro/views.py
@@ -17,6 +17,10 @@ def home3(request):
     return HttpResponse("Ta strona to jest super!")
 
 
+def home4(request):
+    return HttpResponse("Witam urzytkownika!")
+
+
 def name_view(request, name):
     msg = f"Cześć, {name}!"
     return HttpResponse(msg)
